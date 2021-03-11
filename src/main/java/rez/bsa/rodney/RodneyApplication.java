42a2533297191d8bbe6c4423b0725780520408cc
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2021-03-11 14:24:11 -0500

    initial commit

---
+++ src/main/java/rez/bsa/rodney/RodneyApplication.java
@@ -0,0 +1,16 @@
+package rez.bsa.rodney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class RodneyApplication {
+
+    public static
+    void main(String[] args) {
+        SpringApplication.run(RodneyApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/rez/bsa/rodney/RodneyApplicationTests.java
@@ -0,0 +1,13 @@
+package rez.bsa.rodney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
